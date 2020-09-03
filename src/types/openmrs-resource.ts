4bd295f49ec1202ccf87d4de7fc1c98b387938b3
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2020-09-03 13:36:05 +0200

    Removed now unused members.

--- src/types/openmrs-resource.ts
+++ src/types/openmrs-resource.ts
@@ -3,25 +3,3 @@ export interface OpenmrsResource {
   display?: string;
   [anythingElse: string]: any;
 }
-
-export type SessionUser = {
-  allowedLocales: string[];
-  authenticated: boolean;
-  locale: string;
-  sessionId: string;
-  user: User;
-  currentProvider: { uuid: string; identifier: string };
-  sessionLocation: any | null;
-};
-
-type User = {
-  display: string;
-  link: string[];
-  persion: any;
-  priviliges: any;
-  resourceVersion: any;
-  roles: any[];
-  userProperties: any;
-  username: string;
-  uuid: string;
-};
--- src/widgets/shared-utils.tsx
+++ src/widgets/shared-utils.tsx
@@ -8,11 +8,6 @@ export function openWorkspaceTab<TProps = DataCaptureComponentProps, TParams = a
   throw new Error('TODO! Restore visit functionality.');
 }
 
-export function capitalize(s): string {
-  if (typeof s !== 'string') return '';
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-
 export type DataCaptureComponentProps = {
   entryStarted: () => void;
   entrySubmitted: () => void;
