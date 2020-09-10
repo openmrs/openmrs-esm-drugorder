ca4b53dbc6641c09d1d0c4e14d1ca37ca1af0546
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2020-09-10 12:12:50 +0200

    Make the routing work for the local dev setup. Probably requires additional work in the future.

--- src/widgets/medications/medications.component.tsx
+++ src/widgets/medications/medications.component.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import MedicationsDetailedSummary from './medications-detailed-summary.component';
 import MedicationRecord from './medication-record.component';
 
 function Medications(props) {
-  const match = useRouteMatch();
   return (
     <Switch>
-      {/* // TODO: Evaluate whether the first route should match exact.
-        // Currently removed in order to make local development possible. */}
-      <Route path={match.path}>
+      <Route exact path="/drugorder/patient/:patientUuid">
         <MedicationsDetailedSummary />
       </Route>
-      <Route exact path={`${match.path}/:medicationUuid`}>
+      <Route exact path="/drugorder/patient/:patientUuid/medication-orders/:medicationUuid">
         <MedicationRecord />
       </Route>
     </Switch>
