6281f238b24bff0ebc6e65875c6a86ba892775bb
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2020-09-10 12:15:10 +0200

    Remove unnecessary router in dev-extension-slot.

--- src/dev-extension-slot.tsx
+++ src/dev-extension-slot.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
 import { ExtensionSlotReact } from '@openmrs/esm-extension-manager';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 const { bootstrap, mount, unmount } = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: () => {
     return (
-      <BrowserRouter basename={window['getOpenmrsSpaBase']()}>
-        <Route
-          path="/drugorder"
-          component={() => (
-            <main className="omrs-main-content">
-              <ExtensionSlotReact extensionSlotName="patientChartWidgets" />
-            </main>
-          )}
-        />
-      </BrowserRouter>
+      <main className="omrs-main-content">
+        <ExtensionSlotReact extensionSlotName="patientChartWidgets" />
+      </main>
     );
   },
 });
