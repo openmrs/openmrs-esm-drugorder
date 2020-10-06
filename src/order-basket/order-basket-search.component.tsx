bb15a21321fe0a7b2e92abdcafd95a494151fb54
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2020-10-06 15:36:21 +0200

    Improved search in backend.

--- src/order-basket/order-basket-search.component.tsx
+++ src/order-basket/order-basket-search.component.tsx
@@ -1,11 +1,10 @@
-import React, { useRef, useState } from 'react';
-import { Button, Link, Search, Table, TableBody, TableCell, TableRow, Tile } from 'carbon-components-react';
+import React, { useEffect, useState } from 'react';
+import { Button, Link, Search, Tile } from 'carbon-components-react';
 import { useTranslation } from 'react-i18next';
 import styles from './order-basket-search.scss';
 import { Drug, getDrugByName } from '../utils/medications.resource';
 import { createErrorHandler } from '@openmrs/esm-error-handling';
-import { Idea16, Medication16, ShoppingBag16, Close16 } from '@carbon/icons-react';
-import _ from 'lodash-es';
+import { Medication16, ShoppingBag16 } from '@carbon/icons-react';
 import OrderBasketSearchSuggestions from './order-basket-search-suggestions';
 
 export interface OrderBasketSearchProps {
@@ -18,14 +17,14 @@ export default function OrderBasketSearch({ onDrugSelected }: OrderBasketSearchP
   const [searchResults, setSearchResults] = useState<Array<Drug>>([]);
   const [showSuggestionsPopup, setShowSuggestionsPopup] = useState(false);
 
-  const doSearch = _.debounce(() => {
-    getDrugByName(searchTerm).then(response => setSearchResults(response.data.results), createErrorHandler);
-  }, 300);
-
-  const handleSearchTermChanged = (newSearchTerm: string) => {
-    setSearchTerm(newSearchTerm);
-    doSearch();
-  };
+  useEffect(() => {
+    const abortController = new AbortController();
+    getDrugByName(searchTerm, abortController).then(
+      response => setSearchResults(response.data.results),
+      createErrorHandler,
+    );
+    return () => abortController.abort();
+  }, [searchTerm]);
 
   const handleSearchResultClicked = (drug: Drug) => {
     setSearchTerm('');
@@ -40,7 +39,7 @@ export default function OrderBasketSearch({ onDrugSelected }: OrderBasketSearchP
           value={searchTerm}
           placeHolderText={t('searchFieldPlaceholder', 'Search for an order (e.g. "Aspirin")')}
           labelText={t('searchFieldPlaceholder', 'Search for an order (e.g. "Aspirin")')}
-          onChange={e => handleSearchTermChanged(e.currentTarget?.value ?? '')}
+          onChange={e => setSearchTerm(e.currentTarget?.value ?? '')}
           onFocus={() => setShowSuggestionsPopup(true)}
         />
         {showSuggestionsPopup && (
