3f275295093e40780081ef243fd82c2b25a1f56e
Florian Rappl <florian.rappl@smapiot.com>
2021-02-15 17:25:21 +0100

    Switched to use framework

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { registerBreadcrumbs } from '@openmrs/esm-api';
-import { defineConfigSchema } from '@openmrs/esm-config';
-import { getAsyncLifecycle } from '@openmrs/esm-react-utils';
+import { registerBreadcrumbs, defineConfigSchema, getAsyncLifecycle } from '@openmrs/esm-framework';
 import { backendDependencies } from './openmrs-backend-dependencies';
 
 const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
